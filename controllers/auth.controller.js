a0e4d785a3706c0fd28dd860bea90aa3b8c8f23c
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2023-06-26 09:41:30 +0600

    add code in controller and service folder

---
+++ controllers/auth.controller.js
@@ -0,0 +1,51 @@
+const authService = require("../services/auth.service");
+const userValidationUtil = require("../utils/user.validation.util");
+const { AppError } = require("../utils/error.handler.util");
+const contentNegotiation = require("../utils/content-negotiation.util");
+
+
+exports.registerUser = async (req, res, next) => {
+
+  try {
+    const body = req.body;
+    const ValidRegistration = userValidationUtil.checkValidRegistration(body);
+    if (!ValidRegistration.valid) {
+      throw new AppError(ValidRegistration.message, 400);
+    }
+
+    const registerUserResponse = await authService.registerUser(body);
+
+    res.cookie("jwt", registerUserResponse.token);
+
+    const clientResponse = { data: registerUserResponse.data, message: "Registration is successful" }
+
+    return contentNegotiation.sendResponseInContentNegotiation(req, res, 201, clientResponse);
+
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.loginUser = async (req, res, next) => {
+
+  try {
+    const body = req.body;
+    const validLogin = userValidationUtil.checkValidLogin(body);
+    if (!validLogin.valid) {
+      throw new AppError(validLogin.message, 400);
+    }
+
+    const loginUserResponse = await authService.loginUser(body);
+
+    res.cookie("jwt", loginUserResponse);
+
+    const clientResponse = { message: "Login is successful" };
+
+    return contentNegotiation.sendResponseInContentNegotiation(req, res, 200, clientResponse);
+
+  } catch (err) {
+    next(err);
+  }
+};
+
+
---
+++ services/auth.service.js
@@ -0,0 +1,38 @@
+const authUtil = require("../utils/auth.util");
+const userService = require("../services/user.service");
+const { AppError } = require("../utils/error.handler.util");
+
+
+exports.registerUser = async (body) => {
+
+  const newUserResponse = await userService.createUser(body);
+
+  const username = newUserResponse.user.username;
+  const token = await authUtil.generateJwtToken(
+    username
+  );
+  return { data: newUserResponse, token: token };
+};
+
+
+exports.loginUser = async (body) => {
+  const username = body.username;
+  const userResponse = await userService.getUserLoginInfo(username);
+
+  const password = body.password;
+  const user = userResponse;
+  const storedHashPassword = user.password;
+  const isValidPassword = await authUtil.comparePassword(
+    password,
+    storedHashPassword
+  );
+
+  if (isValidPassword) {
+    const username = userResponse.username;
+    const token = await authUtil.generateJwtToken(username);
+    return token;
+  }
+  throw new AppError('Authentication Failed', 401);
+
+};
+
