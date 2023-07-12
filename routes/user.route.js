b669df1293c479b3e3d4803aa6f8d921fe116989
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2023-07-12 14:17:45 +0600

    add intro

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,6 +2,9 @@ const userController = require("../controllers/user.controller");
 const express = require("express");
 const router = express.Router();
 
-router.route("/:email").post(userController.getUserByEmail);
+router.route("/:email").get(userController.getUserByEmail);
+router.route("/intro").get((req,res)=>{
+    res.send("Hi Brain DEV Team");
+})
 
 module.exports = router;
